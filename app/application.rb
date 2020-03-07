375012ba790a5f4368443fc8be39a0fbbfed4665
Christopher Leja <christopher.leja@gmail.com>
2020-03-07 17:30:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,9 @@ def call(env)
   req = Rack::Request.new(env)
   
   if req.path.match(/items/)
-    item = req.path.split("/items")
+    item = req.path.split("/items").last
+    if @@items.include? item
+      
   else
     resp.write "Route not found"
     resp.status = 404
