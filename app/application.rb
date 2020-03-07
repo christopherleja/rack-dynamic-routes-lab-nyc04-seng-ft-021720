70919543c084c278bf18e71792641db36fe29862
Christopher Leja <christopher.leja@gmail.com>
2020-03-07 17:29:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
   
-  if req.path.match"/items"
+  if req.path.match(/items/)
     resp.write "#{price}"
   else
     resp.write "Route not found"
