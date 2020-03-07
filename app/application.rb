94da0671d4486ce0234f65395061904d031b8b03
Christopher Leja <christopher.leja@gmail.com>
2020-03-07 17:40:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ def call(env)
     item_name = req.path.split("/items").last
     if @@items.find{|i| i.name == item_name}
       resp.write item.price
-      
+    
   else
     resp.write "Route not found"
     resp.status = 404
