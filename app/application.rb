83302561bf8a3662094ba90cf74662738f502dd5
Christopher Leja <christopher.leja@gmail.com>
2020-03-07 17:49:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,6 @@ def call(env)
   resp = Rack::Response.new
   req = Rack::Request.new(env)
   
-  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
-  
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
     if item =@@items.find{|i| i.name == item_name}
